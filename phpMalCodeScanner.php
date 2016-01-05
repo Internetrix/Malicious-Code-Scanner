3c703fcac0c046078d3a1039e7dec438cecf5e5e
Jason <jason-zz@users.noreply.github.com>
2016-01-06 09:26:21 +1100

    Change email to irx log email
    Change email to irx log email

--- phpMalCodeScanner.php
+++ phpMalCodeScanner.php
@@ -12,7 +12,7 @@ License: GPL-2
 
 
 // Set to your email:
-define('SEND_EMAIL_ALERTS_TO','youremail@example.com');
+define('SEND_EMAIL_ALERTS_TO','logs@internetrix.com.au');
 
 
 ############################################ START CLASS
